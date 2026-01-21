a59a5a52ace9845f08ac124cfcfa9dc618bed5f8
DPI2-HSS-HP217 <187776668+DPI2-HSS-HP217@users.noreply.github.com>
2026-01-21 15:51:54 +0800

    Rename Duke to Nephilim
    Rename chatbot name and add a skeletal greeting/goodbye message.

---
+++ src/main/java/Nephilim.java
@@ -0,0 +1,16 @@
+public class Nephilim {
+    public static void main(String[] args) {
+        /* String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n"; */
+        String linebrk = "____________________________________________________________";
+        System.out.println(linebrk);
+        System.out.println("Greetings from Nephilim-451.");
+        System.out.println("Please state the nature of your request.");
+        System.out.println(linebrk);
+        System.out.println("Goodbye.");
+        System.out.println(linebrk);
+    }
+}
