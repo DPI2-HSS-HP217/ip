969beee493247cd4468e0b6c6e3ed74cf04f5f07
DPI2-HSS-HP217 <187776668+DPI2-HSS-HP217@users.noreply.github.com>
2026-01-21 16:08:59 +0800

    Add support for echo
    Chatbot will echo user input and exit on "bye" input.

--- src/main/java/Nephilim.java
+++ src/main/java/Nephilim.java
@@ -1,4 +1,7 @@
+import java.util.*;
+
 public class Nephilim {
+
     public static void main(String[] args) {
         /* String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -10,7 +13,14 @@ public class Nephilim {
         System.out.println("Greetings from Nephilim-451.");
         System.out.println("Please state the nature of your request.");
         System.out.println(linebrk);
-        System.out.println("Goodbye.");
-        System.out.println(linebrk);
+
+        Scanner scn = new Scanner(System.in);
+        String command = scn.nextLine();
+        while (!(command.equals("bye"))) {
+            System.out.println(linebrk + '\n' + command + '\n' + linebrk);
+            command = scn.nextLine();
+        }
+        command = "So concludes our conversation.";
+        System.out.println(linebrk + '\n' + command + '\n' + linebrk);
     }
 }
