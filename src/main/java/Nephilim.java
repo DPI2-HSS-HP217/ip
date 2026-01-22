a1684d95219f98bcdf91735160901217ab8f97f9
DPI2-HSS-HP217 <187776668+DPI2-HSS-HP217@users.noreply.github.com>
2026-01-22 14:56:03 +0800

    Add list support

--- src/main/java/Nephilim.java
+++ src/main/java/Nephilim.java
@@ -8,19 +8,38 @@ public class Nephilim {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n"; */
+        //Greeting
         String linebrk = "____________________________________________________________";
         System.out.println(linebrk);
         System.out.println("Greetings from Nephilim-451.");
         System.out.println("Please state the nature of your request.");
         System.out.println(linebrk);
 
+        //Setup Variables
+        ArrayList<String> tasks = new ArrayList<>(100);
+
+        //Read commands
         Scanner scn = new Scanner(System.in);
-        String command = scn.nextLine();
-        while (!(command.equals("bye"))) {
-            System.out.println(linebrk + '\n' + command + '\n' + linebrk);
-            command = scn.nextLine();
+        String input = scn.nextLine();
+
+        while (!(input.equals("bye"))) {
+            switch (input) {
+                case "list":
+                    System.out.println(linebrk);
+                    for (int i = 0; i < tasks.size(); i++) {
+                        System.out.println((i + 1) + ". " + tasks.get(i));
+                    }
+                    System.out.println(linebrk);
+                    break;
+                default:
+                    tasks.add(input);
+                    System.out.println(linebrk + '\n' + "Added: " +
+                            input + '\n' + linebrk);
+                    break;
+            }
+            input = scn.nextLine();
         }
-        command = "So concludes our conversation.";
-        System.out.println(linebrk + '\n' + command + '\n' + linebrk);
+        input = "So concludes our conversation.";
+        System.out.println(linebrk + '\n' + input + '\n' + linebrk);
     }
 }
